15d188d668b2c61a0eb2184c4c7437edd89a0cb8
zuri06 <bothcv168@gmail.com>
2026-07-21 11:31:02 +0700

    style: center align DiamondDivider in CoupleInfo component for improved layout consistency

--- src/templates/blue-botanical-v1/components/CoupleInfo.tsx
+++ src/templates/blue-botanical-v1/components/CoupleInfo.tsx
@@ -180,7 +180,7 @@ export const CoupleInfo: React.FC<CoupleInfoProps> = ({ data }) => {
               </div>
             </div>
 
-            <div className="w-1/3">
+            <div className="w-1/3" style={{ margin: "0 auto" }}>
               <DiamondDivider color="#E8C97A" />
             </div>
 
